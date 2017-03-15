f45b6da2191337d5164859e8624b3ca2665c6e41
Nicole Wong <nhswong@gmail.com>
2017-03-15 13:23:27 -0700

    first commit: basic proj template + title component

---
+++ app/containers/App.js
@@ -0,0 +1,41 @@
+import React, { Component, PropTypes } from 'react'
+import { View, ScrollView, StyleSheet, TextInput, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { Actions } from 'react-native-router-flux'
+
+import { actionCreators } from '../redux/todoRedux'
+
+import Title from '../components/Title'
+import List from '../components/List'
+import Footer from '../components/Footer'
+import Input from '../components/Input'
+import Checkbox from '../components/Checkbox'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
+
+const mapStateToProps = (state) => ({
+  items: state.items,
+})
+
+class App extends Component {
+
+  static propTypes = {
+    items: PropTypes.array.isRequired,
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Title>Todo List</Title>
+
+      </View>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(App)
